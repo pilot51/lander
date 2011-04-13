5c597a21e8d8d6df7e8b9a01ca40c1aeb7256f55
Pilot_51 <pilota51@gmail.com>
2011-04-13 09:33:47 -0400

    Redesign lander_layout.xml, primarily HUD/buttons.
    (Mostly code redesign and some visual)
    -Put HUD and buttons in separate RelativeLayouts.
    -Change HUD text color to white.
    -Add margins of 10dp above and below HUD.

--- gen/com/pilot51/lander/R.java
+++ gen/com/pilot51/lander/R.java
@@ -69,16 +69,21 @@ containing a value of this type.
         public static final int thrust=0x7f02001f;
     }
     public static final class id {
-        public static final int btnLeft=0x7f070006;
-        public static final int btnRight=0x7f070005;
-        public static final int btnThrust=0x7f070007;
+        public static final int altitude=0x7f070002;
+        public static final int btnLeft=0x7f07000a;
+        public static final int btnRight=0x7f07000b;
+        public static final int btnThrust=0x7f07000c;
+        public static final int fuel=0x7f070005;
+        public static final int hud=0x7f070001;
         public static final int lander=0x7f070000;
-        public static final int seekbar=0x7f070009;
-        public static final int value=0x7f070008;
-        public static final int valueAlt=0x7f070001;
-        public static final int valueFuel=0x7f070004;
-        public static final int valueVelX=0x7f070002;
-        public static final int valueVelY=0x7f070003;
+        public static final int seekbar=0x7f07000e;
+        public static final int value=0x7f07000d;
+        public static final int valueAlt=0x7f070006;
+        public static final int valueFuel=0x7f070009;
+        public static final int valueVelX=0x7f070007;
+        public static final int valueVelY=0x7f070008;
+        public static final int velocity_x=0x7f070003;
+        public static final int velocity_y=0x7f070004;
     }
     public static final class layout {
         public static final int lander_layout=0x7f030000;
